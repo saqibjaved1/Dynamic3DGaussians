7d958ea4daed15111115610a17e6a5ff8cfa5f0e
Ahmad Jarrar <ahmad.jarrar@outlook.com>
2024-10-10 12:02:09 +0200

    Minor code change for testing

--- test.py
+++ test.py
@@ -23,40 +23,6 @@ from torch.autograd import Variable
 from math import exp
 
 
-RENDER_MODE = 'color'  # 'color', 'depth' or 'centers'
-# RENDER_MODE = 'depth'  # 'color', 'depth' or 'centers'
-# RENDER_MODE = 'centers'  # 'color', 'depth' or 'centers'
-
-ADDITIONAL_LINES = None  # None, 'trajectories' or 'rotations'
-# ADDITIONAL_LINES = 'trajectories'  # None, 'trajectories' or 'rotations'
-# ADDITIONAL_LINES = 'rotations'  # None, 'trajectories' or 'rotations'
-
-REMOVE_BACKGROUND = False  # False or True
-# REMOVE_BACKGROUND = True  # False or True
-
-FORCE_LOOP = False  # False or True
-# FORCE_LOOP = True  # False or True
-
-w, h = 640, 360
-near, far = 0.01, 100.0
-view_scale = 3.9
-fps = 20
-traj_frac = 25  # 4% of points
-traj_length = 15
-def_pix = torch.tensor(
-    np.stack(np.meshgrid(np.arange(w) + 0.5, np.arange(h) + 0.5, 1), -1).reshape(-1, 3)).cuda().float()
-pix_ones = torch.ones(h * w, 1).cuda().float()
-
-
-def init_camera(y_angle=0., center_dist=2.4, cam_height=1.3, f_ratio=0.82):
-    ry = y_angle * np.pi / 180
-    w2c = np.array([[np.cos(ry), 0., -np.sin(ry), 0.],
-                    [0.,         1., 0.,          cam_height],
-                    [np.sin(ry), 0., np.cos(ry),  center_dist],
-                    [0.,         0., 0.,          1.]])
-    k = np.array([[f_ratio * w, 0, w / 2], [0, f_ratio * w, h / 2], [0, 0, 1]])
-    return w2c, k
-
 def mse(img1, img2):
     return (((img1 - img2)) ** 2).view(img1.shape[0], -1).mean(1, keepdim=True)
 
@@ -113,69 +79,40 @@ def _ssim(img1, img2, window, window_size, channel, size_average=True):
         return ssim_map.mean(1).mean(1).mean(1)
     
 
-def load_scene_data(seq, exp, seg_as_col=False):
+def load_scene_data(seq, exp, masking=False, prune=False, seg_as_col=False):
     params = dict(np.load(f"./output/{exp}/{seq}/params.npz"))
     params = {k: torch.tensor(v).cuda().float() for k, v in params.items()}
     is_fg = params['seg_colors'][:, 0] > 0.5
     scene_data = []
     for t in range(len(params['means3D'])):
-        mask = ((torch.sigmoid(params["mask"][t]) > 0.01).float() - torch.sigmoid(params["mask"][t])).detach() + torch.sigmoid(params["mask"][t])
-        rendervar = {
-            'means3D': params['means3D'][t],
-            'colors_precomp': params['rgb_colors'][t] if not seg_as_col else params['seg_colors'],
-            'rotations': torch.nn.functional.normalize(params['unnorm_rotations'][t]),
-            'opacities': torch.sigmoid(params['logit_opacities']) * mask,
-            'scales': torch.exp(params['log_scales']) * mask,
-            'means2D': torch.zeros_like(params['means3D'][0], device="cuda")
-        }
-        if REMOVE_BACKGROUND:
-            rendervar = {k: v[is_fg] for k, v in rendervar.items()}
-        scene_data.append(rendervar)
-    if REMOVE_BACKGROUND:
-        is_fg = is_fg[is_fg]
+        if not masking:
+            rendervar = {
+                'means3D': params['means3D'][t],
+                'colors_precomp': params['rgb_colors'][t] if not seg_as_col else params['seg_colors'],
+                'rotations': torch.nn.functional.normalize(params['unnorm_rotations'][t]),
+                'opacities': torch.sigmoid(params['logit_opacities']),
+                'scales': torch.exp(params['log_scales']),
+                'means2D': torch.zeros_like(params['means3D'][0], device="cuda")
+            }
+            scene_data.append(rendervar)
+        else:
+            mask = torch.sigmoid(params["mask"][t])
+            
+            if not prune:
+                rendervar = {
+                    mask = ((mask > 0.01).float() - mask).detach() + mask
+                    'means3D': params['means3D'][t],
+                    'colors_precomp': params['rgb_colors'][t] if not seg_as_col else params['seg_colors'],
+                    'rotations': torch.nn.functional.normalize(params['unnorm_rotations'][t]),
+                    'opacities': torch.sigmoid(params['logit_opacities']) * mask,
+                    'scales': torch.exp(params['log_scales']) * mask,
+                    'means2D': torch.zeros_like(params['means3D'][0], device="cuda")
+                }
+                scene_data.append(rendervar)
+            else:
+                print("Pruning Not Implemented")
+                exit(0)
     return scene_data, is_fg
-
-
-def make_lineset(all_pts, cols, num_lines):
-    linesets = []
-    for pts in all_pts:
-        lineset = o3d.geometry.LineSet()
-        lineset.points = o3d.utility.Vector3dVector(np.ascontiguousarray(pts, np.float64))
-        lineset.colors = o3d.utility.Vector3dVector(np.ascontiguousarray(cols, np.float64))
-        pt_indices = np.arange(len(lineset.points))
-        line_indices = np.stack((pt_indices, pt_indices - num_lines), -1)[num_lines:]
-        lineset.lines = o3d.utility.Vector2iVector(np.ascontiguousarray(line_indices, np.int32))
-        linesets.append(lineset)
-    return linesets
-
-
-def calculate_trajectories(scene_data, is_fg):
-    in_pts = [data['means3D'][is_fg][::traj_frac].contiguous().float().cpu().numpy() for data in scene_data]
-    num_lines = len(in_pts[0])
-    cols = np.repeat(colormap[np.arange(len(in_pts[0])) % len(colormap)][None], traj_length, 0).reshape(-1, 3)
-    out_pts = []
-    for t in range(len(in_pts))[traj_length:]:
-        out_pts.append(np.array(in_pts[t - traj_length:t + 1]).reshape(-1, 3))
-    return make_lineset(out_pts, cols, num_lines)
-
-
-def calculate_rot_vec(scene_data, is_fg):
-    in_pts = [data['means3D'][is_fg][::traj_frac].contiguous().float().cpu().numpy() for data in scene_data]
-    in_rotation = [data['rotations'][is_fg][::traj_frac] for data in scene_data]
-    num_lines = len(in_pts[0])
-    cols = colormap[np.arange(num_lines) % len(colormap)]
-    inv_init_q = deepcopy(in_rotation[0])
-    inv_init_q[:, 1:] = -1 * inv_init_q[:, 1:]
-    inv_init_q = inv_init_q / (inv_init_q ** 2).sum(-1)[:, None]
-    init_vec = np.array([-0.1, 0, 0])
-    out_pts = []
-    for t in range(len(in_pts)):
-        cam_rel_qs = quat_mult(in_rotation[t], inv_init_q)
-        rot = build_rotation(cam_rel_qs).cpu().numpy()
-        vec = (rot @ init_vec[None, :, None]).squeeze()
-        out_pts.append(np.concatenate((in_pts[t] + vec, in_pts[t]), 0))
-    return make_lineset(out_pts, cols, num_lines)
-
 
 def render(w2c, k, timestep_data):
     with torch.no_grad():
@@ -188,49 +125,29 @@ def render(cam, timestep_data):
         im, _, depth, = Renderer(raster_settings=cam)(**timestep_data)
         return im, depth
 
-
-def rgbd2pcd(im, depth, w2c, k, show_depth=False, project_to_cam_w_scale=None):
-    d_near = 1.5
-    d_far = 6
-    invk = torch.inverse(torch.tensor(k).cuda().float())
-    c2w = torch.inverse(torch.tensor(w2c).cuda().float())
-    radial_depth = depth[0].reshape(-1)
-    def_rays = (invk @ def_pix.T).T
-    def_radial_rays = def_rays / torch.linalg.norm(def_rays, ord=2, dim=-1)[:, None]
-    pts_cam = def_radial_rays * radial_depth[:, None]
-    z_depth = pts_cam[:, 2]
-    if project_to_cam_w_scale is not None:
-        pts_cam = project_to_cam_w_scale * pts_cam / z_depth[:, None]
-    pts4 = torch.concat((pts_cam, pix_ones), 1)
-    pts = (c2w @ pts4.T).T[:, :3]
-    if show_depth:
-        cols = ((z_depth - d_near) / (d_far - d_near))[:, None].repeat(1, 3)
-    else:
-        cols = torch.permute(im, (1, 2, 0)).reshape(-1, 3)
-    pts = o3d.utility.Vector3dVector(pts.contiguous().double().cpu().numpy())
-    cols = o3d.utility.Vector3dVector(cols.contiguous().double().cpu().numpy())
-    return pts, cols
-
 def get_dataset(t, md, seq):
     dataset = []
+    # print("Reading Cameras")
     for c in range(len(md['fn'][t])):
         w, h, k, w2c = md['w'], md['h'], md['k'][t][c], md['w2c'][t][c]
         cam = setup_camera(w, h, k, w2c, near=1.0, far=100)
         fn = md['fn'][t][c]
         im = np.array(copy.deepcopy(Image.open(f"/scratch/cvlab/datasets/datasets_ahmad/panoptic/{seq}/ims/{fn}")))
         im = torch.tensor(im).float().cuda().permute(2, 0, 1) / 255
-        seg = np.array(copy.deepcopy(Image.open(f"/scratch/cvlab/datasets/datasets_ahmad/panoptic/{seq}/seg/{fn.replace('.jpg', '.png')}"))).astype(np.float32)
-        seg = torch.tensor(seg).float().cuda()
-        seg_col = torch.stack((seg, torch.zeros_like(seg), 1 - seg))
-        dataset.append({'cam': cam, 'im': im, 'seg': seg_col, 'id': c})
+        # seg = np.array(copy.deepcopy(Image.open(f"/scratch/cvlab/datasets/datasets_ahmad/panoptic/{seq}/seg/{fn.replace('.jpg', '.png')}"))).astype(np.float32)
+        # seg = torch.tensor(seg).float().cuda()
+        # seg_col = torch.stack((seg, torch.zeros_like(seg), 1 - seg))
+        dataset.append({'cam': cam, 'im': im, 'id': c})
     return dataset
 
 def test(seq, exp):
-
+    print(f"Testing Experiment: {exp}, {seq}")
+    
+    print("Loading Metadata")
     md = json.load(open(f"/scratch/cvlab/datasets/datasets_ahmad/panoptic/{seq}/test_meta.json", 'r'))  # metadata
     num_timesteps = len(md['fn'])
 
-
+    print("Loading Gaussian Model")
     scene_data, is_fg = load_scene_data(seq, exp)
 
     render_path = f"./output/{exp}/{seq}/renders"
@@ -244,9 +161,10 @@ def test(seq, exp):
     psnrs = []
     lpipss = []
 
-
+    print("Starting Testing...")
     for t in tqdm(range(num_timesteps)):
         dataset = get_dataset(t, md, seq)
+        # print("Testing")
         for camera in dataset:
             idx = camera['id']
             im, depth = render(camera["cam"], scene_data[t])
@@ -255,13 +173,14 @@ def test(seq, exp):
             torchvision.utils.save_image(im, os.path.join(render_path, 't_{0:05d}_{0:05d}'.format(t, idx) + ".png"))
             torchvision.utils.save_image(gt, os.path.join(gts_path, 't_{0:05d}_{0:05d}'.format(t, idx) + ".png"))
             ssims.append(ssim(im, gt))
-            psnrs.append(psnr(im, gt))
+            psnrs.append(psnr(im, gt).mean())
             lpipss.append(lpips(im, gt, net_type='vgg'))
 
-
+    print("  # Gaussians: {}".format(len(scene_data[t]['means3D']))
     print("  SSIM : {:>12.7f}".format(torch.tensor(ssims).mean(), ".5"))
     print("  PSNR : {:>12.7f}".format(torch.tensor(psnrs).mean(), ".5"))
     print("  LPIPS: {:>12.7f}".format(torch.tensor(lpipss).mean(), ".5"))
+
 
 
 
